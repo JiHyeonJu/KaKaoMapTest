1a1be7d2546c8c46e7c06961b0f9fe8be87f44fc
jjh <jjh7639@naver.com>
2021-08-03 20:24:04 +0900

    remove unused code

--- app/src/main/java/com/kakao/android/kakaomaptest/ui/MainActivity.kt
+++ app/src/main/java/com/kakao/android/kakaomaptest/ui/MainActivity.kt
@@ -1,133 +1,23 @@
 package com.kakao.android.kakaomaptest.ui
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Base64
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.material.snackbar.Snackbar
 import com.kakao.android.kakaomaptest.R
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
-    val REQUEST_CODE = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getHashKey()
-
-        //isLocationPermissionGranted(findViewById(R.id.main_view));
+        //getHashKey()
     }
-
-    /*fun setDataAtListFragment(category: String) {
-        val bundle = Bundle()
-        bundle.putString("itemName", category)
-
-        val transaction = supportFragmentManager?.beginTransaction()
-        var fragment = MapFragment()
-        fragment.arguments = bundle
-        if (transaction != null) {
-            transaction.replace(R.id.fragment_map, fragment)
-            transaction.commit()
-        }
-    }*/
-
-    private fun isLocationPermissionGranted(container: View): Boolean {
-        val preference = getPreferences(Context.MODE_PRIVATE)
-        val isFirstCheck = preference.getBoolean(
-            "isFirstPermissionCheck",
-            true
-        )
-
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            ) {
-                // 거부만 한 경우 사용자에게 왜 필요한지 이유를 설명해주는게 좋다
-                val snackBar = Snackbar.make(
-                    container,
-                    "suggest_permissison_grant",
-                    Snackbar.LENGTH_INDEFINITE
-                )
-                snackBar.setAction("권한승인") {
-                    ActivityCompat.requestPermissions(
-                        this,
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        REQUEST_CODE
-                    )
-                }
-                snackBar.show()
-            } else {
-                if (isFirstCheck) { // 처음 물었는지 여부를 저장
-                    preference.edit().putBoolean("isFirstPermissionCheck", false)
-                        .apply() // 권한요청
-                    ActivityCompat.requestPermissions(
-                        this,
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        REQUEST_CODE
-                    )
-                } else {
-                    // 사용자가 권한을 거부하면서 다시 묻지않음 옵션을 선택한 경우 // requestPermission을 요청해도 창이 나타나지 않기 때문에 설정창으로 이동한다.
-                    val snackBar = Snackbar.make(
-                        container,
-                        "suggest_permissison_grant_in_setting",
-                        Snackbar.LENGTH_INDEFINITE
-                    )
-                    snackBar.setAction("확인") {
-                        val intent = Intent()
-                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                        val uri = Uri.fromParts(
-                            "package",
-                            packageName,
-                            null
-                        )
-                        intent.data = uri
-                        startActivity(intent)
-                    }
-                    snackBar.show()
-                }
-            }
-            return false
-        } else {
-            return true
-        }
-    }
-
-
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }*/
-
-    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }*/
 
     private fun getHashKey() {
         var packageInfo: PackageInfo? = null
